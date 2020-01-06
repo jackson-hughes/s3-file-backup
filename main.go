4c3f029257c72a1ad94321e8a438b5f1bda50794
Jackson Hughes <jxhughes95@gmail.com>
2020-01-06 23:36:48 +0000

    Add early but functional S3 upload

--- main.go
+++ main.go
@@ -2,13 +2,14 @@
 mvp needs:
 takes input for filename / pattern
 validate input
-todo: takes input for destination bucket
+takes input for destination bucket
 
 nice to haves:
 todo: boolean for delete from disk after backup
 todo: compress prior to upload if not already compressed
 todo: sends event to Cloud watch events with exit status (success, error)
 todo: no-op / dry run mode - logs what would have happened
+todo: optionally prefix new s3 objects (e.g. today's date)
 */
 
 package main
@@ -17,6 +18,12 @@ import (
 	"flag"
 	"os"
 	"path/filepath"
+
+	"github.com/aws/aws-sdk-go/aws"
+
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+
+	"github.com/aws/aws-sdk-go/aws/session"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -55,7 +62,34 @@ func main() {
 		log.Debug(f)
 	} else {
 		log.Infof("No files found matching pattern: %v", filepattern)
+		os.Exit(0)
 	}
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	uploader := s3manager.NewUploader(sess)
+	// log.Info(f[1])
+	fileToUpload, err := os.Open(f[0])
+	if err != nil {
+		log.Error(err)
+	}
+	defer fileToUpload.Close()
+	r, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(backupBucket),
+		Key:    aws.String("file.txt"),
+		Body:   fileToUpload,
+	})
+	if err != nil {
+		log.Error(err)
+	}
+	log.Infof("file uploaded to, %s", r.Location)
+	// for _, file := range f {
+	// 	err := uploadToS3(file)
+	// 	if err != nil {
+	// 		log.Error(err)
+	// 	}
+	// }
 }
 
 func checkFlags() {
@@ -74,8 +108,11 @@ func checkFlags() {
 	}
 }
 
-func uploadToS3() {
-	log.Info("S3")
+func uploadToS3(file string) (err error) {
+
+	log.Debugf("Uploading %v to %v", file, backupBucket)
+
+	return
 }
 
 func findFile(pattern string) (match []string, err error) {
